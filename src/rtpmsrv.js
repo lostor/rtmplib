0961ba07b74bd333b2de2c7ace0417b162fa8180
Ilya (notebook) <Ilya (notebook)@localhost>
2011-07-21 21:57:01 +0300

    Started!

---
+++ src/rtpmsrv.js
@@ -0,0 +1,20 @@
+var net = require('net');
+ 
+var server = net.createServer(function (stream) {
+    stream.setEncoding('utf8');
+ 
+    stream.addListener('connect', function () {
+        console.log("Connect!");
+    });
+ 
+    stream.addListener('data', function (data) {
+        console.log(data.toString('ascii'));
+    });
+ 
+    stream.addListener('end', function () {
+        console.log("Disconnect!");
+        stream.end();
+    });
+});
+
+server.listen(1935, "127.0.0.1");
